1dbb05beb4991ee4c9412e852f7b0bf4c5c89e6c
David Bartalis <bartalisdavid.98@gmail.com>
2021-02-01 09:44:58 +0000

    apricot_exp package ready

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='apricotexp',
+      version='0.0.1',
+      description="Example package",
+      url='',
+      author='Dávid Bartalis',
+      author_email='bartalisdavid.98@gmail.com',
+      packages=['apricot_exp'],
+      install_requires=[
+          'numpy',
+          'pandas',
+          'matplotlib',
+          'seaborn',
+          'comet-ml',
+      ],
+      zip_safe=False
+)
